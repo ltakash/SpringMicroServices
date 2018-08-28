86eb6004e9501c797279e8548dc819935144f460
AkashSinghal <AkashSinghal@DESKTOP-PFDDD4K>
2018-08-28 17:31:24 +0530

    added file to get data from url dynamically deleted applicaiton.properties and replace it with bootstrap.properties

--- LimitsService/src/main/java/com/limit/config/ConfigurationProp.java
+++ LimitsService/src/main/java/com/limit/config/ConfigurationProp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limits")
+@ConfigurationProperties("limits.service")
 public class ConfigurationProp {
 int max;
 int min;
